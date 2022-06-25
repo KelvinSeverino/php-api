bc9cc73c10cff09237dd133474797ee5a96a27e2
Kelvin Severino <kseveri@outlook.com>
2022-06-25 10:06:09 -0300

    Inclusão das config de acesso ao BD

---
+++ source/Boot/Config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * BANCO DE DADOS
+ */
+//Valores globais de conexão com o BD
+define("CONF_DB_HOST", "localhost");
+define("CONF_DB_PORT", "3306");
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", "");
+define("CONF_DB_NAME", "api");

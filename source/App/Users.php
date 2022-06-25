a0aa64e031388744941038c6a8cccbf2a3dca437
Kelvin Severino <kseveri@outlook.com>
2022-06-25 10:05:55 -0300

    Criação das Controladoras

---
+++ source/App/Users.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Source\App;
+
+class Users
+{
+    public function get()
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+        
+    }
+}

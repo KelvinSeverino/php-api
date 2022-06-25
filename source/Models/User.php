bcd4e09650674db02b68963f3918676c35d7d904
Kelvin Severino <kseveri@outlook.com>
2022-06-25 10:05:33 -0300

    Criação da Models

---
+++ source/Models/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Source\Models;
+
+use PDO;
+use PDOException;
+
+class User
+{
+    private static $table = 'users';
+
+    public static function getUser(int $idUser)
+    {
+        //Realizando conexao com o BD
+        try{
+            $connPdo = new PDO('mysql:host='.CONF_DB_HOST.';dbname=' . CONF_DB_NAME, CONF_DB_USER, CONF_DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));	
+        } catch ( PDOException $e ) {
+            echo "Erro: " . $e->getMessage() . " <br/>";
+        }        
+
+        $sql = 'SELECT * FROM '.self::$table.' WHERE id = :id';
+
+        $stmt = $connPdo->prepare($sql);
+        $stmt->bindValue(':id', $idUser);
+        $stmt->execute();
+
+        if($stmt->rowCount() > 0)
+        {
+            return $stmt->fetch(\PDO::FETCH_ASSOC);
+        }
+        else
+        {
+            throw new \Exception("Nenhum User encontrado!");
+        }
+    }
+}

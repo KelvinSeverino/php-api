a8e9e2651c19d0daa43a6c493007cc3de1ab20f7
Kelvin Severino <kseveri@outlook.com>
2022-06-26 10:34:39 -0300

    Correção no script

--- database/script.sql
+++ database/script.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,  
   `email` varchar(200) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,  
-  `password` varchar(300) NOT NULL,
+  `nome` varchar(100) DEFAULT NULL,  
+  `senha` varchar(300) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` (`id`, `email`, `name`, `password`) VALUES
+INSERT INTO `users` (`id`, `email`, `nome`, `senha`) VALUES
 (1, 'kelvin@email.com', '1234', 'Kelvin');
 COMMIT;
